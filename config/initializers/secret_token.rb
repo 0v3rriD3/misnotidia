76e23c81761cbaaa0c588a7ec25cd077a4d41787
k4ru050 <khalebpr@gmail.com>
2011-03-06 19:11:50 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Misnotidia::Application.config.secret_token = 'eeb64dad3e2a6bfb5a0164f9d2b8aeb770e52a2763113f4f55b14c3e94ea5e236f57d5614bb09190cf7a60d654baa55bd78eec47dcf8ab2a5ebe9c8301c1b8c4'
